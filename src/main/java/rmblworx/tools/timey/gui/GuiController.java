59d63128e20fbe9563394a116ebc02bf48e76777
Christian Raue <christian.raue@gmail.com>
2014-02-06 21:43:57 +0100

    Tabs und Stopp-Schaltfläche

--- src/main/java/rmblworx/tools/timey/gui/GuiController.java
+++ src/main/java/rmblworx/tools/timey/gui/GuiController.java
@@ -16,14 +16,31 @@ public class GuiController {
 	private Button stopwatchStartButton;
 
 	@FXML
+	private Button stopwatchStopButton;
+
+	@FXML
 	void initialize() {
-		assert stopwatchStartButton != null : "fx:id=\"stopwatchStartButton\" was not injected: check your FXML file 'TimeyGui.fxml'.";
+		assert stopwatchStartButton != null : "fx:id='stopwatchStartButton' was not injected: check your FXML file 'TimeyGui.fxml'.";
+		assert stopwatchStopButton != null : "fx:id='stopwatchStopButton' was not injected: check your FXML file 'TimeyGui.fxml'.";
 
 		if (stopwatchStartButton != null) {
 			stopwatchStartButton.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent event) {
 					System.out.println(resources.getString("stopwatchStartButton.pressed"));
+					stopwatchStartButton.setVisible(false);
+					stopwatchStopButton.setVisible(true);
+				}
+			});
+		}
+
+		if (stopwatchStopButton != null) {
+			stopwatchStopButton.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					System.out.println(resources.getString("stopwatchStopButton.pressed"));
+					stopwatchStartButton.setVisible(true);
+					stopwatchStopButton.setVisible(false);
 				}
 			});
 		}
