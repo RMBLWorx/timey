c5066abfcd46a13858979313d83d58717206dbea
mmatthies <wwwSuchMichMalDe@web.de>
2014-03-27 22:24:35 +0100

    + TimeyEventDispatcher zur Verwaltung und Verteilung der Events

---
+++ src/main/java/rmblworx/tools/timey/TimeyEventDispatcher.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package rmblworx.tools.timey;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Verwaltet die Listener und verteilt die Events.
+ * @author mmatthies
+ */
+public class TimeyEventDispatcher {
+	private List<TimeyEventListener> listener = new LinkedList<>();
+
+	public synchronized void addEventListener(TimeyEventListener timeyEventListener) {
+		this.listener.add(timeyEventListener);
+	}
+
+	public synchronized void removeEventListener(TimeyEventListener timeyEventListener) {
+		this.listener.remove(timeyEventListener);
+	}
+
+	private synchronized void dispatchEvent(TimeyEvent timeyEvent) {
+		Iterator<TimeyEventListener> it = this.listener.iterator();
+		while (it.hasNext()) {
+			it.next().handleEvent(timeyEvent);
+		}
+	}
+}
