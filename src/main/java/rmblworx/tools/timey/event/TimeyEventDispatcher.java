3c99634252c90c88d98034088868d536a264cd38
mmatthies <wwwSuchMichMalDe@web.de>
2014-07-18 22:48:55 +0200

    # Workaround - doppelt/dreifache TimeyEventDispatcher Referenzen (eine pro Kontext) nun rudimentär behoben

--- src/main/java/rmblworx/tools/timey/event/TimeyEventDispatcher.java
+++ src/main/java/rmblworx/tools/timey/event/TimeyEventDispatcher.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 /**
  * Verwaltet die Listener und verteilt die Events.
- * 
+ *
  * @author mmatthies
  */
 public class TimeyEventDispatcher {
+	private static TimeyEventDispatcher instance;
 	/**
 	 * Liste aller registrierten Event-Listener.
 	 */
@@ -17,7 +18,7 @@ public class TimeyEventDispatcher {
 
 	/**
 	 * Registriert bei Dispatcher eine Listener-Implementierung.
-	 * 
+	 *
 	 * @param timeyEventListener
 	 *            Referenz auf die Listener-Implementierung
 	 */
@@ -28,8 +29,9 @@ public class TimeyEventDispatcher {
 	/**
 	 * Macht die Registrierung einer Listener-Implementierung wieder rueckgaengig. Der Listener wird fortan nicht mehr
 	 * benachrichtigt.
-	 * 
-	 * @param timeyEventListener Referenz auf die Listener-Implementierung
+	 *
+	 * @param timeyEventListener
+	 *            Referenz auf die Listener-Implementierung
 	 */
 	public synchronized void removeEventListener(final TimeyEventListener timeyEventListener) {
 		this.listener.remove(timeyEventListener);
@@ -37,12 +39,29 @@ public class TimeyEventDispatcher {
 
 	/**
 	 * Verteilt/ benachrichtigt alle registrierten Listener ueber das uebergebene Event.
-	 * @param timeyEvent das Event
+	 *
+	 * @param timeyEvent
+	 *            das Event
 	 */
 	public synchronized void dispatchEvent(final TimeyEvent timeyEvent) {
 		Iterator<TimeyEventListener> it = this.listener.iterator();
 		while (it.hasNext()) {
 			it.next().handleEvent(timeyEvent);
 		}
+	}
+
+	/*
+	 * TODO
+	 * Diese Fabrikmethode dient derzeit als Workaround. Da die getrennten ApplicationContexte jedesmal eine eigene
+	 * EventDispatcher-Instanz bilden wird dies hiermit explizit unterbunden. Der saubere Ansatz waere den
+	 * EventDispatcher in einer Parent xml zu deklarieren und in den jeweiligen Alarm- und Countdown-Spring-XML's darauf
+	 * zu referenzieren - folgt noch
+	 */
+	public static synchronized TimeyEventDispatcher getInstance() {
+		if (instance == null) {
+			instance = new TimeyEventDispatcher();
+		}
+
+		return instance;
 	}
 }
