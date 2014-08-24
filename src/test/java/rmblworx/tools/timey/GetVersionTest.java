217ddedfa1171b610675f2b8333d017762d41206
Christian Raue <christian.raue@gmail.com>
2014-08-24 17:46:24 +0200

    Versionskennung wird in separater Datei hinterlegt und kann einfacher abgerufen werden als bisher

--- src/test/java/rmblworx/tools/timey/GetVersionTest.java
+++ src/test/java/rmblworx/tools/timey/GetVersionTest.java
@@ -2,8 +2,6 @@ package rmblworx.tools.timey;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.io.IOException;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,9 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import rmblworx.tools.timey.exception.EmptyArgumentException;
-import rmblworx.tools.timey.exception.NullArgumentException;
 
 /*
  * Copyright 2014 Christian Raue
@@ -47,15 +42,10 @@ public class GetVersionTest {
 	}
 
 	/**
-	 * Test method for {@link rmblworx.tools.timey.TimeyFacade#getVersion(String)}.
-	 *
-	 * @throws Exception
-	 * @throws IllegalStateException
-	 * @throws NullArgumentException
-	 * @throws EmptyArgumentException
+	 * Test method for {@link rmblworx.tools.timey.TimeyFacade#getVersion()}.
 	 */
 	@Test
-	public final void testGetVersion() throws IllegalStateException, NullArgumentException, EmptyArgumentException {
+	public final void testGetVersion() {
 		if (TimeyUtils.isWindowsSystem()) {
 			LOG.debug("Windows erkannt...");
 		} else if (TimeyUtils.isLinuxSystem()) {
@@ -64,22 +54,7 @@ public class GetVersionTest {
 			LOG.debug("OS X erkannt...");
 		}
 
-		try {
-			TestHelper.executeMavenPackageWithoutRunningTestsProcess();
-		} catch (final IOException e) {
-			LOG.error(e.getLocalizedMessage());
-			return; // Test überspringen
-		}
-
-		/*
-		 * Da die jar erst nach erfolgreichem maven build existiert, besteht
-		 * hier ein klassisches Henne-Ei-Problem denn um erfolgreich zu bauen,
-		 * muessen alle Tests erfolgreich durchlaufen worden sein! Deshalb wird hier explizit gebaut ohne die Tests
-		 * auszufuehren um im Anschluss daran den Versionstest durchfuehren zu können. .
-		 */
-
-		assertNotNull("Test fehlgeschlagen da keine Version zurueckgeliefert wurde.",
-				this.facade.getVersion("timey.jar"));
+		assertNotNull("Test fehlgeschlagen da keine Version zurueckgeliefert wurde.", this.facade.getVersion());
 	}
 
 }
