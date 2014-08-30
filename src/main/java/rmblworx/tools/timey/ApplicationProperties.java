5ccd24a22db462ef1c6d1b0d5490837379a093cb
mmatthies <wwwSuchMichMalDe@web.de>
2014-08-30 15:04:07 +0200

    # Ergänzung des ApplicationPropertiesTest um die Abdeckung zu erhöhen - es wird IOException forciert und auf Rückgabe von null getestett

--- src/main/java/rmblworx/tools/timey/ApplicationProperties.java
+++ src/main/java/rmblworx/tools/timey/ApplicationProperties.java
@@ -13,14 +13,10 @@ import org.springframework.core.io.ClassPathResource;
  */
 /**
  * Zugriff auf Attribute der Anwendung.
+ *
  * @author Christian Raue {@literal <christian.raue@gmail.com>}
  */
 class ApplicationProperties {
-
-	/**
-	 * Name der Datei.
-	 */
-	private static final String PROPERTY_FILENAME = "app.properties";
 
 	/**
 	 * Name des Attributs für die Versionskennung.
@@ -28,28 +24,59 @@ class ApplicationProperties {
 	private static final String PROP_APP_VERSION = "application.version";
 
 	/**
-	 * @return Versionskennung
+	 * Name der Datei.
+	 */
+	private static final String PROPERTY_FILENAME = "app.properties";
+	private InputStream inputStream;
+
+	/**
+	 * Properties.
+	 */
+	private Properties properties;
+
+	/**
+	 * Standardkonstruktor.
+	 */
+	public ApplicationProperties() {
+	}
+
+	/**
+	 * Erweiterter Konstruktor. Lediglich für Testzwecke.
+	 *
+	 * @param properties
+	 *            Referenz auf die Properties-Instanz.
+	 */
+	ApplicationProperties(final Properties properties, final InputStream inputStream) {
+		this.properties = properties;
+		this.inputStream = inputStream;
+	}
+
+	/**
+	 * @return Versionskennung oder null wenn beim auslesen der Version eine Ausnahme auftrat.
 	 */
 	public String getVersion() {
-		InputStream inputStream = null;
 		try {
-			inputStream = new ClassPathResource(PROPERTY_FILENAME).getInputStream();
+			if (this.inputStream == null) {
+				this.inputStream = new ClassPathResource(PROPERTY_FILENAME).getInputStream();
+			}
 
-			final Properties props = new Properties();
-			props.load(inputStream);
-			return props.getProperty(PROP_APP_VERSION);
+			if (this.properties == null) {
+				this.properties = new Properties();
+			}
+			this.properties.load(this.inputStream);
+			return this.properties.getProperty(PROP_APP_VERSION);
 		} catch (final IOException e) {
-			LoggerFactory.getLogger(getClass()).error("Error while trying to read the property file: " + e.getLocalizedMessage());
+			LoggerFactory.getLogger(this.getClass()).error(
+					"Error while trying to read the property file: " + e.getLocalizedMessage());
 			return null;
 		} finally {
-			if (inputStream != null) {
+			if (this.inputStream != null) {
 				try {
-					inputStream.close();
+					this.inputStream.close();
 				} catch (final IOException e) {
 					// ignorieren
 				}
 			}
 		}
 	}
-
 }
--- src/test/java/rmblworx/tools/timey/ApplicationPropertiesTest.java
+++ src/test/java/rmblworx/tools/timey/ApplicationPropertiesTest.java
@@ -2,8 +2,18 @@ package rmblworx.tools.timey;
 
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 /*
  * Copyright 2014 Christian Raue
@@ -11,9 +21,42 @@ import org.junit.Test;
  */
 /**
  * Test fürs Auslesen der Anwendungsattribute.
+ *
  * @author Christian Raue {@literal <christian.raue@gmail.com>}
  */
 public class ApplicationPropertiesTest {
+	@Mock
+	private InputStream inputStream;
+	@Mock
+	private Properties props;
+
+	@Before
+	public void setup() throws IOException {
+		MockitoAnnotations.initMocks(this);
+		// this.inputStream = new ClassPathResource("app.properties").getInputStream();
+		this.setupMockedInputStream();
+		this.setupMockedProperties();
+	}
+
+	/**
+	 * Setup-Methode für das zu mockende Properties-Objekt.
+	 *
+	 * @throws IOException
+	 *             wenn IOException auftritt.
+	 */
+	@SuppressWarnings("unchecked")
+	private void setupMockedInputStream() throws IOException {
+		// when(this.inputStream.close()).thenThrow(IOException.class);
+		doThrow(new IOException()).when(this.inputStream).close();
+	}
+
+	/**
+	 * Setup-Methode für das zu mockende Properties-Objekt.
+	 */
+	@SuppressWarnings("unchecked")
+	private void setupMockedProperties() {
+		when(this.props.getProperty("application.version")).thenThrow(IOException.class);
+	}
 
 	/**
 	 * Testet das Auslesen der Versionskennung.
@@ -25,6 +68,16 @@ public class ApplicationPropertiesTest {
 		assertNotNull(version);
 		assertNotEquals("", version);
 		assertNotEquals("${project.version}", version);
+	}
+
+	/**
+	 * Löst gezielt eine IOException aus um das korrekte Verhalten der Methode zu testen. Erwartet wird lediglich eine
+	 * Fehlermeldung im Log und die Rückgabe von <code>null</code>.
+	 */
+	@Test
+	public final void testGetVersionShouldReturnNullIfIOExceptionOccurs() {
+		assertNull("Es wurde eine Referenz geliefert!",
+				new ApplicationProperties(this.props, this.inputStream).getVersion());
 	}
 
 }
